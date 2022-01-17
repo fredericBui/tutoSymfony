88eecbe69f3eb5c5046621820f78ee99080764b9
Frederic <bui.frederic.pro@gmail.com>
2022-01-17 22:59:53 +0100

    Afficher les données d'un formulaire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +13,24 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $form=$this->createForm(ContactType::class);
-        return $this->renderForm('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-            'formulaire' => $form
-        ]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&& $form->isValid()){
+            $data=$form->getData();
+            $email=$data['email'];
+
+            return $this->render('contact/success.html.twig', [
+                'email' => $email
+            ]);
+        }else{
+            return $this->renderForm('contact/index.html.twig', [
+                'controller_name' => 'ContactController',
+                'formulaire' => $form
+            ]);
+        }
+        
     }
 }
