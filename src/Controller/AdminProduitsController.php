37aa717ac89d4429ccd207f07f325b6afd308fea
Frederic <bui.frederic.pro@gmail.com>
2022-01-18 23:25:41 +0100

    Creer un espace d'administration d'entites produits

---
+++ src/Controller/AdminProduitsController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produits;
+use App\Form\ProduitsType;
+use App\Repository\ProduitsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/produits")
+ */
+class AdminProduitsController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_produits_index", methods={"GET"})
+     */
+    public function index(ProduitsRepository $produitsRepository): Response
+    {
+        return $this->render('admin_produits/index.html.twig', [
+            'produits' => $produitsRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="admin_produits_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $produit = new Produits();
+        $form = $this->createForm(ProduitsType::class, $produit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($produit);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_produits_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_produits/new.html.twig', [
+            'produit' => $produit,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_produits_show", methods={"GET"})
+     */
+    public function show(Produits $produit): Response
+    {
+        return $this->render('admin_produits/show.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_produits_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Produits $produit, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProduitsType::class, $produit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_produits_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_produits/edit.html.twig', [
+            'produit' => $produit,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_produits_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Produits $produit, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$produit->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($produit);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_produits_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
