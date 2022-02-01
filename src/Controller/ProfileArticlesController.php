05e96e880ae8bb7dd97d110979a1e68a9b1d6f76
Frederic <bui.frederic.pro@gmail.com>
2022-02-01 22:48:48 +0100

    espace utilisateur gestion article

---
+++ src/Controller/ProfileArticlesController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Articles;
+use App\Form\ArticlesType;
+use App\Repository\ArticlesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/profile/articles")
+ */
+class ProfileArticlesController extends AbstractController
+{
+    /**
+     * @Route("/", name="profile_articles_index", methods={"GET"})
+     */
+    public function index(ArticlesRepository $articlesRepository): Response
+    {
+        return $this->render('profile_articles/index.html.twig', [
+            'articles' => $articlesRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="profile_articles_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $article = new Articles();
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('profile_articles_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profile_articles/new.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="profile_articles_show", methods={"GET"})
+     */
+    public function show(Articles $article): Response
+    {
+        return $this->render('profile_articles/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="profile_articles_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Articles $article, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('profile_articles_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profile_articles/edit.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="profile_articles_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Articles $article, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('profile_articles_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
